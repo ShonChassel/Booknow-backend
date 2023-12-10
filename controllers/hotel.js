d46582975fca0af566ba26c9f43e0ea77a5c13b1
Shon Chassel <114100392+ShonChassel@users.noreply.github.com>
2023-12-10 13:10:10 +0200

    Update hotel.js
    Update  countByCity fun

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -54,18 +54,28 @@ export const getHotels = async (req, res, next) => {
 };
 
 export const countByCity = async (req, res, next) => {
-    const cities = req.query.cities.split(",")
+  const cities = req.query.cities.split(",");
 
-    try {
-        const list = await Promise.all(cities.map(city => {
-            return Hotel.countDocuments({ city: city })
-        }))
+  try {
+    let list;
 
-        res.status(200).json(list)
-    } catch (err) {
-        next(err)
+    list = await Promise.all(cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+
+    if (!list) {
+      list = await Promise.all(cities.map((country) => {
+          return Hotel.countDocuments({ country: country });
+        })
+      );
     }
-}
+
+    res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const countByType = async (req, res, next) => {
     try {
@@ -102,4 +112,19 @@ export const getHotelRooms = async (req, res, next) => {
         next(err);
     }
 };
+
+
+// export const countByCity = async (req, res, next) => {
+//     const cities = req.query.cities.split(",")
+
+//     try {
+//         const list = await Promise.all(cities.map(city => {
+//             return Hotel.countDocuments({ city: city })
+//         }))
+
+//         res.status(200).json(list)
+//     } catch (err) {
+//         next(err)
+//     }
+// }
 
